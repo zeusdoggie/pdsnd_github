160be4580a555db1075c5d4c6ac35d343f3b59d2
Dan Hicks <hicksdan5571@gmail.com>
2022-10-18 16:07:58 -0700

    Round runtime seconds to 4 places

--- bikeshare.py
+++ bikeshare.py
@@ -18,10 +18,10 @@ def get_filters():
     """
     print('Hello! Let\'s explore some US bikeshare data!')
     # TO DO: get user input for city (chicago, new york city, washington). HINT: Use a while loop to handle invalid inputs
-    
+
     # Get City from user to determine file to process
     city = None
-    
+
     while city != 'None':
         city = input('Enter a city to analyze the data. (Chicago, New York City or Washington): ')
         city = city.lower()
@@ -30,10 +30,10 @@ def get_filters():
         if city == None:
             print('\nNot a valid city. Please enter a valid city.\n')
         else:
-            break            
-        
+            break
+
     # TO DO: get user input for month (all, january, february, ... , june)
-    
+
     #Get Month from user to determine Month filter
 
     while True:
@@ -44,7 +44,7 @@ def get_filters():
             month = months.index(month)
             if month == 0:
                 month = 'all'
-            
+
             break
         #trap for keyboard interupt to exit program without processing data
         except KeyboardInterrupt:
@@ -52,7 +52,7 @@ def get_filters():
             raise SystemExit
         except:
             print('\nNot a valid month. Please enter a valid month.\n')
-            
+
     # TO DO: get user input for day of week (all, monday, tuesday, ... sunday)
     #Get day of the week from user to determine Day filter
     day = None
@@ -73,10 +73,10 @@ def get_filters():
         if day == None:
             print('\nNot a valid day. Please enter a valid day.\n')
         else:
-            break            
-        
+            break
+
     print('\nData File: {} - Month Filter: {} - Day Filter: {}'.format(city, month, day))
-    
+
     print('-'*40)
     return city, month, day
 
@@ -98,11 +98,11 @@ def load_data(city, month, day):
     # convert Start and End Time columns to datetime
     df['Start Time'] = pd.to_datetime(df['Start Time'])
     df['End Time'] = pd.to_datetime(df['End Time'])
-    
+
     # extract month and day of week from Start Time to create new columns
     df['month'] = df['Start Time'].dt.month
     df['day_of_week'] = df['Start Time'].dt.day_name()
-    
+
     # filter by month if applicable
     if month != 'all':
         # filter by month to create the new dataframe
@@ -125,7 +125,7 @@ def time_stats(df):
     # TO DO: display the most common month
     # Calculate most common month and convert to a month name
     common_month_name = df['Start Time'].dt.month_name().mode()[0]
-    print('Most common month:', common_month_name)    
+    print('Most common month:', common_month_name)
 
     # TO DO: display the most common day of week
     # Calculate most common day of the week
@@ -136,8 +136,8 @@ def time_stats(df):
     # Calculate the most common hour
     common_hour = df['Start Time'].dt.hour.mode()[0]
     print('Most common hour: ', common_hour)
-    
-    print("\nThis took %s seconds." % (time.time() - start_time))
+
+    print("\nThis took %s seconds." % round((time.time() - start_time), 4))
     print('-'*40)
 
 
@@ -162,7 +162,7 @@ def station_stats(df):
     common_trip = (df['Start Station'] + ' and ' + df['End Station']).mode()[0]
     print('Most common trip stations:', common_trip)
 
-    print("\nThis took %s seconds." % (time.time() - start_time))
+    print("\nThis took %s seconds." % round((time.time() - start_time), 4))
     print('-'*40)
 
 
@@ -182,8 +182,8 @@ def trip_duration_stats(df):
     trip_average = int(df['Trip Duration'].mean())
     print('Average trip duration (H:M:S): {}'.format(timedelta(seconds=trip_average)))
 
-    
-    print('\nThis took %s seconds.' % (time.time() - start_time))
+
+    print("\nThis took %s seconds." % round((time.time() - start_time), 4))
     print('-'*40)
 
 
@@ -199,7 +199,7 @@ def user_stats(df):
     print('\nTypes of user counts:', count_type)
 
     # TO DO: Display counts of gender
-    # Calculate counts for gender   
+    # Calculate counts for gender
     try:
         count_gender = df.groupby(['Gender'])['Gender'].count()
         print('\nGender counts:', count_gender)
@@ -211,20 +211,20 @@ def user_stats(df):
     try:
         birth_early = int(df['Birth Year'].min())
         print('\nEarliest birth year:', birth_early)
-    
+
         birth_recent = int(df['Birth Year'].max())
         print('Latest birth year:', birth_recent)
-    
+
         birth_common = int(df['Birth Year'].mode()[0])
         print('Most common birth year:', birth_common)
-        
+
     except KeyError:
         print('\nData not available for Birth Year.')
 
-    print("\nThis took %s seconds." % (time.time() - start_time))
+    print("\nThis took %s seconds." % round((time.time() - start_time), 4))
     print('-'*40)
 
-    
+
 def display_file(city):
     """Displays file."""
     """
@@ -233,7 +233,7 @@ def display_file(city):
     Args:
         (str) city - name of the csv file to display the raw data
     """
-    
+
     print('\nDisplay raw data...\n')
     start_time = time.time()
 
@@ -245,16 +245,16 @@ def display_file(city):
             print(raw_line)
             if (raw_line == ''):
                 print('\nEnd of File.\n')
-                raise SystemExit        
+                raise SystemExit
 
         cont = input('\nWould you like to view more data? Enter yes or no.\n')
         if cont.lower() != 'yes':
             f.close()
             break
 
-    print("\nThis took %s seconds." % (time.time() - start_time))
+    print("\nThis took %s seconds." % round((time.time() - start_time), 4))
     print('-'*40)
-    
+
 
 def main():
     while True:
@@ -265,7 +265,7 @@ def main():
         station_stats(df)
         trip_duration_stats(df)
         user_stats(df)
-        
+
         view_data = input('\nWould you like to view raw data from the file? Enter yes or no.\n')
         if view_data.lower() == 'yes':
             display_file(city)
